3beab53b296316b69037b28548eaf6370e4b3181
PearStone <housenka@outlook.com>
2018-12-19 04:08:28 +0900

    * ログファイル生成機能を本体に組み込み

--- ccounter.php
+++ ccounter.php
@@ -1,9 +1,11 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 class CCounter
 {
     // セーブファイル
     const COUNT_FILE = 'count.log';
-    
+
     // カウントファイルから任意の値を得る
     private function getLog($mode = NULL)
     {
@@ -41,14 +43,14 @@ class CCounter
         $new_today = $this->getLog('t') + 1;
 
         // ファイルが書き込めないときにエラー
-        if (!is_writeable(self::COUNT_FILE)) throw new Exception ('Could not read file.');
+        if (!is_writable(self::COUNT_FILE)) throw new Exception ('Could not read file.');
 
         file_put_contents(self::COUNT_FILE,
                           $new_all . ',' . $new_today . ',' . $this->getLog('y') . ',' . $this->getLog('d'),
                           LOCK_EX);
     }
 
-    // 日付が変わったときのカウンターのリセット
+    // カウンターのリセット
     private function resetLog($now_date = NULL)
     {
         // パラメータがないときにエラー
@@ -58,11 +60,19 @@ class CCounter
         $log_today = $this->getLog('t');
 
         // ファイルが書き込めないときにエラー
-        if (!is_writeable(self::COUNT_FILE)) throw new Exception ('Could not read file.');
+        if (!is_writable(self::COUNT_FILE)) throw new Exception ('Could not read file.');
 
         file_put_contents(self::COUNT_FILE,
                           $log_all . ',' . '0' . ',' . $log_today . ',' . $now_date,
                           LOCK_EX);
+    }
+
+    // ログファイルの初期化
+    private function initLog()
+    {
+        $today_date = date('d');
+        file_put_contents(self::COUNT_FILE, '0,0,0,' . $today_date, LOCK_EX);
+        chmod(self::COUNT_FILE, 0600);
     }
 
     // カウントの出力整形
@@ -76,13 +86,25 @@ class CCounter
     // コンストラクタ
     public function __construct()
     {
+        # ログファイルが不正、または存在しないときにログファイルの初期化
+        // ログファイルがあったら書式を調べ、なかったらログファイル初期化
+        if (file_exists(self::COUNT_FILE)) {
+            $log_data = file_get_contents(self::COUNT_FILE);
+            // 書式が正しくないときに初期化
+            if (!preg_match('/\A\d+,\d+,\d+,\d{1,2}\z/', $log_data)) $this->initLog();
+        } else {
+            $this->initLog();
+        }
+
         try {
+
+
             // 日付が変わったときにカウントリセット
             $today_date = date('d');
             if ($today_date !== $this->getLog('d')) {
                 $this->resetLog($today_date);
             }
-            
+
             // カウントアップ
             $this->incrementCount();
         } catch (Exception $e) {
